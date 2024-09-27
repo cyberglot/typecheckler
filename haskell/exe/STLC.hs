0f1e8f22dfc4826ea3e5abee7dd3af3c25004045
april <april@cyberglot.me>
2024-09-27 10:08:43 +0100

    old updates

--- haskell/exe/STLC.hs
+++ haskell/exe/STLC.hs
@@ -20,6 +20,7 @@ data TCTerm v a = Return a
                 | Failure
 
 instance Functor (TCTerm v) where
+  fmap :: (a -> b) -> TCTerm v a -> TCTerm v b
   fmap f (Return a)     = Return $ f a
   fmap _ (Var i)        = Var i
   fmap f (Lam ty t)     = Lam ty (fmap f t)
@@ -29,7 +30,9 @@ instance Functor (TCTerm v) where
   fmap _ Failure        = Failure
 
 instance Applicative (TCTerm v) where
-  pure x = Return x
+  pure :: a -> TCTerm v a
+  (<*>) :: TCTerm v (a -> b) -> TCTerm v a -> TCTerm v b
+  pure = Return
   Return f     <*> a = fmap f a
   Var i        <*> _ = Var i
   Lam ty f     <*> a = Lam ty (f <*> a)
@@ -39,6 +42,7 @@ instance Applicative (TCTerm v) where
   Failure      <*> _ = Failure
 
 instance Monad (TCTerm v) where
+  (>>=) :: TCTerm v a -> (a -> TCTerm v b) -> TCTerm v b
   Return a     >>= f = f a
   Var i        >>= _ = Var i
   Lam ty t     >>= f = Lam ty (t >>= f)
@@ -48,7 +52,7 @@ instance Monad (TCTerm v) where
   Failure      >>= _ = Failure
 
 assumption :: v -> TCTerm v a
-assumption v = Var v
+assumption = Var
 
 introduce :: Type -> TCTerm v a
 introduce a = let v = undefined in Lam a (Return v) -- `v` has to be introduced somewhere
@@ -63,42 +67,61 @@ eval (HasType a t) = hasType a (eval t)
 eval Failure       = failure
 
 instance Typechecker ([Type] -> Maybe Type) where
-  var i = \types -> Just (types !! i) -- I hate this
-  lam ty f = \types -> case f (ty : types) of
+  var :: Int -> [Type] -> Maybe Type
+  var i types = Just (types !! i) -- I hate this
+
+  lam :: Type -> ([Type] -> Maybe Type) -> [Type] -> Maybe Type
+  lam ty f types = case f (ty : types) of
                         Just ty' -> Just (Fun ty ty')
                         _        -> Nothing
-  app tm1 tm2 = \types -> case (tm1 types, tm2 types) of
+
+  app :: ([Type] -> Maybe Type) -> ([Type] -> Maybe Type) -> [Type] -> Maybe Type
+  app tm1 tm2 types = case (tm1 types, tm2 types) of
                           (Just (Fun t t'), Just ty) -> if t == ty then Just t' else Nothing
                           _                          -> Nothing
-  have i t f = \types -> case types !! i == t of
-                          True  -> f types
-                          False -> Nothing
-  hasType t f = \types -> case f types of
-                            Just ty -> if ty == t then Just ty else Nothing
-                            _ -> Nothing
-  failure = \_ -> Nothing
+
+  have :: Int -> Type -> ([Type] -> Maybe Type) -> [Type] -> Maybe Type
+  have i t f types = if types !! i == t then f types else Nothing
+
+  hasType :: Type -> ([Type] -> Maybe Type) -> [Type] -> Maybe Type
+  hasType t f types = case f types of
+                        Just ty -> if ty == t then Just ty else Nothing
+                        _ -> Nothing
+
+  failure :: [Type] -> Maybe Type
+  failure _ = Nothing
 
 type Elab = [Type] -> Maybe (Type, TCTerm [Type] Type)
 
-instance Typechecker (Elab) where
-  var i = \types ->
+instance Typechecker Elab where
+  var :: Int -> Elab
+  var i types =
     let t = types !! i
     in Just (t, Var [t])
-  lam ty f = \types ->
-    case f (ty : types) of
-      Just (ty', tm) -> Just (Fun ty ty', Lam ty tm)
-      _              -> Nothing
-  app tm1 tm2 = \types ->
+
+  lam :: Type -> Elab -> Elab
+  lam ty f types = case f (ty : types) of
+                     Just (ty', tm) -> Just (Fun ty ty', Lam ty tm)
+                     _              -> Nothing
+
+  app :: Elab -> Elab -> Elab
+  app tm1 tm2 types =
     case (tm1 types, tm2 types) of
       (Just (Fun t t', tm'1), Just (ty, tm'2)) -> if t == ty then Just (t', App tm'1 tm'2) else Nothing
       _                                      -> Nothing
-  have i t f = \types ->
-    case types !! i == t of
-      True  -> let Just (ty, tm) = f types
-               in Just (ty, Have i t tm)
-      False -> Nothing
-  hasType t f = \types ->
+
+  have :: Int -> Type -> Elab -> Elab
+  have i t f types =
+    if types !! i == t
+    then let Just (ty, tm) = f types
+         in Just (ty, Have i t tm)
+    else Nothing
+
+  hasType :: Type -> Elab -> Elab
+  hasType t f types =
     case f types of
       Just (ty, tm) -> if ty == t then Just (ty, HasType ty tm) else Nothing
       _ -> Nothing
-  failure = \_ -> Nothing
+
+  failure :: Elab
+  failure _ = Nothing
